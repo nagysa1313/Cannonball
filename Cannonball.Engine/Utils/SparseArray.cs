53642665d2af7510418e3c24bf85be79db65596b
Nagy Sándor <nagysa1313@gmail.com>
2014-10-16 00:01:31 +0200

    Added sparse array implementation

---
+++ Cannonball.Engine/Utils/SparseArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cannonball.Engine.Utils
+{
+    public class SparseArray<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        private Dictionary<TKey, TValue> dictionary;
+        private TValue defaultValue;
+
+        public SparseArray()
+        {
+            this.defaultValue = default(TValue);
+            dictionary = new Dictionary<TKey, TValue>();
+        }
+        public SparseArray(TValue defaultValue)
+        {
+            this.defaultValue = defaultValue;
+            dictionary = new Dictionary<TKey, TValue>();
+        }
+
+        public TValue this[TKey index]
+        {
+            get
+            {
+                TValue retVal = defaultValue;
+                dictionary.TryGetValue(index, out retVal);
+                return retVal;
+            }
+            set 
+            {
+                if (dictionary.ContainsKey(index))
+                {
+                    dictionary[index] = value;
+                }
+                else
+                {
+                    dictionary.Add(index, value);
+                }
+            }
+        }
+
+        #region IEnumerable<KeyValuePair<TKey, TValue>> members
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return dictionary.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return dictionary.GetEnumerator();
+        }
+        #endregion
+    }
+
+    public class SparseArray<TValue> : SparseArray<int, TValue>
+    {
+
+    }
+}
